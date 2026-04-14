e8fe554f97ca24cb4d58572d2092ef9be07e6582
SREERAMADASU MUKUNDA RAMA CHARY <25mc3060@rgipt.ac.in>
2026-04-14 20:02:22 +0530

    Refine position titles for team members
    Updated position titles for Sai Badrishwar S S and Mukunda Ramachary.

--- src/pages/footer/ContactPage.tsx
+++ src/pages/footer/ContactPage.tsx
@@ -84,9 +84,8 @@ Reach out to our support team, and we'll assist you as quickly as possible.`;
     { id: 1, src: "/figures/1.jpeg", name: "Karri Kishan", position: "Founder & CEO" },
     { id: 2, src: "/figures/2.jpeg", name: "Bhargav Venkat", position: "Co-Founder and Founding Engineer" },
     { id: 3, src: "/figures/3.jpeg", name: "Yaswanth Buduru", position: "CTO and Co-Founder" },
-    { id: 4, src: "/figures/4.jpeg", name: "Sai Badrishwar S S", position: "CMO" },
-    
-    { id: 6, src: "/figures/6.jpeg", name: "Mukunda Ramachary", position: "CIO" },
+    { id: 4, src: "/figures/4.jpeg", name: "Sai Badrishwar S S", position: "Chief Marketing officer (CMO)" },
+    { id: 6, src: "/figures/6.jpeg", name: "Sreeramadasu Mukunda Rama Chary", position: "Chief Information Officer (CIO)" },
   ];
 
   // Placeholder data for the rest of your team
